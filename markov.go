a163aea2e983267556cbba121b327a30274d99ff
Jeromy <why@ipfs.io>
2016-06-28 12:11:15 -0700

    add in code

---
+++ markov.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+var WordSet map[string]*Node
+
+func init() {
+	WordSet = make(map[string]*Node)
+}
+
+type Node struct {
+	Value string
+	Next  []*Link
+	Final int
+}
+
+type Link struct {
+	Weight int
+	Target *Node
+}
+
+func (n *Node) InsertPhrase(p string) {
+	n.Insert(strings.Fields(p))
+}
+
+func (n *Node) Insert(words []string) {
+	if len(words) == 0 {
+		n.Final++
+		return
+	}
+
+	for _, l := range n.Next {
+		if l.Target.Value == words[0] {
+			l.Weight++
+			l.Target.Insert(words[1:])
+			return
+		}
+	}
+
+	var next *Node
+	if nd, ok := WordSet[words[0]]; ok {
+		next = nd
+	} else {
+		next = &Node{
+			Value: words[0],
+		}
+		WordSet[words[0]] = next
+	}
+
+	n.Next = append(n.Next, &Link{
+		Weight: 1,
+		Target: next,
+	})
+
+	next.Insert(words[1:])
+}
+
+func (n *Node) Generate() []string {
+	return n.generate(nil)
+}
+
+func (n *Node) GeneratePhrase() string {
+	return strings.Join(n.Generate(), " ")
+}
+
+func (n *Node) selectNext() *Node {
+	total := n.Final
+	for _, l := range n.Next {
+		total += l.Weight
+	}
+
+	i := rand.Intn(total)
+	if i < n.Final {
+		return nil
+	}
+
+	i -= n.Final
+	for _, l := range n.Next {
+		if i < l.Weight {
+			return l.Target
+		}
+		i -= l.Weight
+	}
+
+	panic("shouldnt get here")
+}
+
+func (n *Node) generate(cur []string) []string {
+	l := n.selectNext()
+	if l == nil {
+		return cur
+	}
+
+	return l.generate(append(cur, l.Value))
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("must pass in corpus file")
+		return
+	}
+
+	infi := os.Args[1]
+	fi, err := os.Open(infi)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+
+	n := new(Node)
+	scan := bufio.NewScanner(fi)
+	for scan.Scan() {
+		n.InsertPhrase(scan.Text())
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 20; i++ {
+		fmt.Println(n.GeneratePhrase())
+	}
+}
